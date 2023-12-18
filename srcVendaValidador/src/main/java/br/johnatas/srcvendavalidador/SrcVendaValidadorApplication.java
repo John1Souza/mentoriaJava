370e95a58f7d42f2893a0e2db05788a6c221b1c9
John1Souza <johnatas2014@hotmail.com>
2023-12-18 14:28:24 -0300

    [NEW] First commit

---
+++ srcVendaValidador/src/main/java/br/johnatas/srcvendavalidador/SrcVendaValidadorApplication.java
@@ -0,0 +1,13 @@
+package br.johnatas.srcvendavalidador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrcVendaValidadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrcVendaValidadorApplication.class, args);
+	}
+
+}
---
+++ srcVendaValidador/src/test/java/br/johnatas/srcvendavalidador/SrcVendaValidadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.johnatas.srcvendavalidador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrcVendaValidadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
